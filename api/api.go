8dd30814a71af81f379ad70b91ace1cdce6ca4cc
Jeremy Storer <jeremyjstorer@gmail.com>
2018-05-08 14:47:45 -0400

    .

--- api/api.go
+++ api/api.go
@@ -27,7 +27,7 @@ func Initialize(isTesting bool) {
 			{"A12T-4GH7-QPL9-3N4M", "Lettuce", "$3.46"},
 			{"E5T6-9UI3-TH15-QR88", "Peach", "$2.99"},
 			{"YRT6-72AS-K736-L4AR", "Green Pepper", "$0.79"},
-			{"TQ4C-VV6T-75ZX-1RMR", "Gala Applessss", "$3.59"},
+			{"TQ4C-VV6T-75ZX-1RMR", "Gala Apple", "$3.59"},
 		}
 	}
 }
