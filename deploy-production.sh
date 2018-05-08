9ad438a59dedd4f32ebac725af06bcfa0a92c35c
Jeremy Storer <jeremyjstorer@gmail.com>
2018-05-07 23:45:35 -0400

    kubernetes test

--- deploy-production.sh
+++ deploy-production.sh
@@ -9,17 +9,17 @@ gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
 gcloud --quiet config set project $PROJECT_NAME_PRD
 gcloud --quiet config set container/cluster $CLUSTER_NAME_PRD
-gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
+gcloud --quiet config set compute/zone $CLOUDSDK_COMPUTE_ZONE
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME_PRD
 
-gcloud docker push gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}
+gcloud docker push gcr.io/$CLUSTER_NAME_PRD/$DOCKER_IMAGE_NAME
 
-yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}:latest
+yes | gcloud beta container images add-tag gcr.io/$CLUSTER_NAME_PRD/$DOCKER_IMAGE_NAME:$TRAVIS_COMMIT gcr.io/$CLUSTER_NAME_PRD/$DOCKER_IMAGE_NAME:latest
 
 kubectl config view
 kubectl config current-context
 
-kubectl set image deployment/${KUBE_DEPLOYMENT_NAME} ${KUBE_DEPLOYMENT_CONTAINER_NAME}=gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT
+kubectl set image deployment/$KUBE_DEPLOYMENT_NAME $KUBE_DEPLOYMENT_CONTAINER_NAME=gcr.io/$CLUSTER_NAME_PRD/$DOCKER_IMAGE_NAME:$TRAVIS_COMMIT
 
 #
 # sleep 30
